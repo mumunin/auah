f3cd4d347adea931c78aa23466e809c0e402ed51
mumunin <146727285+mumunin@users.noreply.github.com>
2024-10-11 06:39:49 +0700

    Update jajan.js

--- jajan.js
+++ jajan.js
@@ -96,7 +96,7 @@ initializePlayer('tel', 'https://live-oneapp-prd-news.akamaized.net/Content/CMAF
 initializePlayer('te4', 'https://cors-proxy.cooks.fyi/https://director.streaming.telia.com/tvm-packager-prod/group3/60ae452e1522bdf301694494/manifest.mpd', {'c1c657aa160c5b328f1e76747534b913':'785e0773e9d534a00f4619315674ef7f'});
     
 // SSC
-initializePlayer('ssc1', 'https://ssc1-ak.akamaized.net/out/v1/c696e4819b55414388a1a487e8a45ca1/index.mpd', {'d84c325f36814f39bbe59080272b10c3': '550727de4c96ef1ecff874905493580f'});
+initializePlayer('ssc1', 'https://ssc-1-enc.edgenextcdn.net/out/v1/c696e4819b55414388a1a487e8a45ca1/index.mpd', {'d84c325f36814f39bbe59080272b10c3': '550727de4c96ef1ecff874905493580f'});
 initializePlayer('ssc2', 'https://ssc2-ak.akamaized.net/out/v1/a16db2ec338a445a82d9c541cc9293f9/index.mpd', {'8bcfc55359e24bd7ad1c5560a96ddd3c': 'b5dcf721ab522af92a9d3bf0bd55b596'});
 initializePlayer('ssc3', 'https://ssc3-ak.akamaized.net/out/v1/42e86125555242aaa2a12056832e7814/index.mpd', {'7de5dd08ad8041d586c2f16ccc9490a1': '5e1503f3398b34f5099933fedab847ef'});
 initializePlayer('ssc4', 'https://ssc4-ak.akamaized.net/out/v1/5267ea5772874b0db24559d643eaad93/index.mpd', {'5c672f6b85a94638872d0214f7806ed4': 'bf8756fbb866ee2d5c701c2289dd8de3'});
